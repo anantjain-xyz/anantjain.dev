37dd2527c7e28a19d1830bd54859d8c5b43eb4e3
Anant Jain <anant90@gmail.com>
2024-02-18 19:50:26 -0800

    Move to slug/index.md format

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -10,12 +10,11 @@ export function getPostSlugs() {
 }
 
 export function getPostBySlug(slug: string) {
-  const realSlug = slug.replace(/\.md$/, "");
-  const fullPath = join(postsDirectory, `${realSlug}.md`);
+  const fullPath = join(postsDirectory, `${slug}/index.md`);
   const fileContents = fs.readFileSync(fullPath, "utf8");
   const { data, content } = matter(fileContents);
 
-  return { ...data, slug: realSlug, content } as Post;
+  return { ...data, slug, content } as Post;
 }
 
 export function getAllPosts(): Post[] {
