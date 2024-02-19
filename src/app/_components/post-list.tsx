b7a5d98ec215b050e5348ad61cbb053becf041df
Anant Jain <anant90@gmail.com>
2024-02-18 18:20:32 -0800

    Clean up PostList

--- src/app/_components/post-list.tsx
+++ src/app/_components/post-list.tsx
@@ -8,14 +8,13 @@ type Props = {
 export function PostList({ posts }: Props) {
   return (
     <section>
-      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
+      <div className="grid grid-cols-1 gap-y-20 mb-32">
         {posts.map((post) => (
           <PostPreview
             key={post.slug}
             title={post.title}
             date={post.date}
             slug={post.slug}
-            excerpt={post.excerpt}
           />
         ))}
       </div>
--- src/app/_components/post-preview.tsx
+++ src/app/_components/post-preview.tsx
@@ -1,16 +1,13 @@
-import { type Author } from "@/interfaces/author";
 import Link from "next/link";
-import CoverImage from "./cover-image";
 import DateFormatter from "./date-formatter";
 
 type Props = {
   title: string;
   date: string;
-  excerpt: string;
   slug: string;
 };
 
-export function PostPreview({ title, date, excerpt, slug }: Props) {
+export function PostPreview({ title, date, slug }: Props) {
   return (
     <div>
       <h3 className="text-3xl mb-3 leading-snug">
@@ -25,7 +22,6 @@ export function PostPreview({ title, date, excerpt, slug }: Props) {
       <div className="text-lg mb-4">
         <DateFormatter dateString={date} />
       </div>
-      <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
     </div>
   );
 }
