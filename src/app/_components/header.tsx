93708c0a138a1091597b3adc6fb936fd0a1aa3bd
Anant Jain <anant90@gmail.com>
2024-02-19 21:52:23 -0800

    Add CSS Globals

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -3,7 +3,10 @@ import Link from "next/link";
 const Header = () => {
   return (
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
-      <Link href="/" className="hover:underline">
+      <Link
+        href="/"
+        className="text-gray-900 hover:text-blue-600 transition-colors duration-200 ease-in-out"
+      >
         Anant Jain
       </Link>
     </h2>
--- src/app/_components/post-preview.tsx
+++ src/app/_components/post-preview.tsx
@@ -14,7 +14,7 @@ export function PostPreview({ title, date, slug }: Props) {
         <Link
           as={`/posts/${slug}`}
           href="/posts/[slug]"
-          className="hover:text-blue-600 transition-colors duration-200 ease-in-out"
+          className="text-gray-900 hover:text-blue-600 transition-colors duration-200 ease-in-out"
         >
           {title}
         </Link>
