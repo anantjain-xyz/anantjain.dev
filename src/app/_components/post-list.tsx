a0d1f41b0fc7669ca76eef120e2f83a5d41a89b2
Anant Jain <anant90@gmail.com>
2024-02-19 17:04:18 -0800

    Add Categories to Home page

--- src/app/_components/post-list.tsx
+++ src/app/_components/post-list.tsx
@@ -2,12 +2,14 @@ import { Post } from "@/interfaces/post";
 import { PostPreview } from "./post-preview";
 
 type Props = {
+  title: string;
   posts: Post[];
 };
 
-export function PostList({ posts }: Props) {
+export function PostList({ title, posts }: Props) {
   return (
-    <section>
+    <section className="mb-16">
+      <h3 className="text-3xl font-bold mb-8">{title}</h3>
       {posts.map((post) => (
         <PostPreview
           key={post.slug}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,63 @@ import { getAllPosts } from "../lib/api";
 export default function Index() {
   const allPosts = getAllPosts();
 
+  const categories = [
+    { tagName: "Tech", displayName: "Tech & Startups", slugName: "tech" },
+    {
+      tagName: "Conference",
+      displayName: "Tech Talks",
+      slugName: "tech-talks",
+    },
+    { tagName: "Interview", displayName: "Interviews", slugName: "interviews" },
+    {
+      tagName: "AI",
+      displayName: "Machine Learning & Artificial Intelligence",
+      shortDisplayName: "ML & AI",
+      slugName: "ai",
+    },
+    {
+      tagName: "UX",
+      displayName: "Product Design",
+      shortDisplayName: "Product Design",
+      slugName: "ux",
+    },
+    {
+      tagName: "Paper Review",
+      displayName: "Paper Reviews",
+      slugName: "paper-reviews",
+    },
+    { tagName: "Book List", displayName: "Book Lists", slugName: "book-lists" },
+    {
+      tagName: "Book Review",
+      displayName: "Book Reviews",
+      slugName: "book-reviews",
+    },
+    {
+      tagName: "Notes",
+      displayName: "Notes",
+      slugName: "notes",
+    },
+    {
+      tagName: "Side Project",
+      displayName: "Side Projects",
+      slugName: "side-projects",
+    },
+    { tagName: "Fitness", displayName: "Fitness", slugName: "fitness" },
+    { tagName: "Art", displayName: "Art", slugName: "art" },
+    { tagName: "Misc", displayName: "Miscellaneous", slugName: "misc" },
+  ];
+
   return (
     <main>
       <Container>
         <Intro />
-        {allPosts.length > 0 && <PostList posts={allPosts} />}
+        <PostList title="Latest" posts={allPosts.slice(0, 5)} />
+        {categories.map((category) => {
+          const posts = allPosts.filter((post) => {
+            return post.categories.indexOf(category.tagName) > -1;
+          });
+          return <PostList title={category.displayName} posts={posts} />;
+        })}
       </Container>
     </main>
   );
