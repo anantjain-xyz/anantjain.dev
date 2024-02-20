ca3aa1240858845e63bc66089dbdc364b10451b2
Anant Jain <anant90@gmail.com>
2024-02-19 17:04:08 -0800

    Update Intro

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -1,9 +1,16 @@
+import { Bio } from "./bio";
+import { Socials } from "./socials";
+
 export function Intro() {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-5xl font-bold tracking-tighter leading-tight md:pr-8">
-        Anant Jain
-      </h1>
+    <section className="mt-16 mb-16 md:mb-12">
+      <div className="flex flex-col md:flex-row items-center">
+        <h1 className="text-5xl font-bold tracking-tighter leading-tight md:pr-8 flex-">
+          Anant Jain
+        </h1>
+        <Socials />
+      </div>
+      <Bio />
     </section>
   );
 }
