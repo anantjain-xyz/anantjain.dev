7b8096f144c4af0436f3d3a2908d725f27104df9
Anant Jain <anant90@gmail.com>
2024-08-17 06:23:40 -0700

    Add profile pic to home

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -1,16 +1,26 @@
 import { Bio } from "./bio";
 import { Socials } from "./socials";
+import Image from "next/image";
 
 export function Intro() {
   return (
-    <section className="mt-16 mb-16 md:mb-12">
-      <div className="flex flex-col md:flex-row items-center">
-        <h1 className="text-5xl font-bold tracking-tighter leading-tight pb-2 md:pr-8 md:pb-0">
-          Anant Jain
-        </h1>
-        <Socials />
-      </div>
-      <Bio />
-    </section>
+    <div className="mt-16 mb-16 md:mb-12 max-w-6xl flex flex-row items-start">
+      <section className="">
+        <div className="flex flex-col md:flex-row items-center">
+          <h1 className="text-5xl font-bold tracking-tighter leading-tight pb-2 md:pr-8 md:pb-0">
+            Anant Jain
+          </h1>
+          <Socials />
+        </div>
+        <Bio />
+      </section>
+      <Image
+        src="/me.jpg"
+        className="rounded-full hidden md:block ml-12"
+        alt="Anant Jain"
+        width={150}
+        height={150}
+      />
+    </div>
   );
 }
